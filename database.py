d92a0dae07b5c805d1b0a92f14d422035ffb24cd
yuki katayama <yukatayama@icloud.com>
2024-06-20 19:56:44 +0900

    “2024/06/20/18:25”

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = 'postgresql://yuka:postgres@localhost/gasoline_price'
+DATABASE_URL = 'postgresql://postgres:postgres@localhost/mnex'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- import_csv/import_csv_to_postgres.py
+++ import_csv/import_csv_to_postgres.py
@@ -3,10 +3,10 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # CSVファイルのパス
-csv_file_path = '/home/akkunn/csv_import_project/import_csv/MN-.csv'  # 適切なパスに変更
+csv_file_path = '/home/akkunn/MNEX_SQL_TEST/import_csv/MN-.csv'  # 適切なパスに変更
 
 # PostgreSQLのデータベース接続情報
-db_url = 'postgresql://yuka:postgres@localhost/gasoline_price'
+db_url = 'postgresql+psycopg2://postgres:postgres@localhost/mnex'
 
 # エンジンの作成
 engine = create_engine(db_url)
